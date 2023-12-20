a6e307c01208e62186f7974e5896d5c0563d1479
probonopd <probonopd@users.noreply.github.com>
2023-12-20 20:08:18 +0100

    Linuxbrew

--- build.sh
+++ build.sh
@@ -13,16 +13,24 @@ apk add ca-certificates build-base wget git bash clang nasm elfutils-dev flex bi
 autoconf git coreutils automake gawk pkgconfig linux-headers libbsd-dev \
 nasm flex bison libstdc++-dev
 
-# git clone https://github.com/dosemu2/fdpp
-# cd fdpp
-wget https://github.com/dosemu2/fdpp/archive/refs/tags/1.4.tar.gz
-tar -zxf 1.4.tar.gz
-cd fdpp-1.4
+# Build and install Linuxbrew which is needed to build newer versions of fdpp
+apk add curl file gzip libc6-compat ncurses ruby ruby-dbm ruby-etc ruby-irb ruby-json sudo
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"
+PATH=$HOME/.linuxbrew/bin:$HOME/.linuxbrew/sbin:$PATH
+brew update
+brew doctor
+
+# Build FDPP which is a dependency of dosemu2
+git clone https://github.com/dosemu2/fdpp
+cd fdpp
+# wget https://github.com/dosemu2/fdpp/archive/refs/tags/1.4.tar.gz
+# tar -zxf 1.4.tar.gz
+# cd fdpp-1.4
 make -j $(nproc)
 make install
 cd -
  
-# Build
+# Build dosemu2
 git clone https://github.com/dosemu2/dosemu2
 cd dosemu2
 # autoreconf --install -v -I m4
