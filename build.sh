6ac0e05e9969c538151398a4eb1ff1b41f11fc52
probonopd <probonopd@users.noreply.github.com>
2023-12-20 22:12:09 +0100

    Fix desktop fie and icon

--- build.sh
+++ build.sh
@@ -11,7 +11,8 @@ fi
 apk update
 apk add ca-certificates build-base wget git bash clang nasm elfutils-dev flex bison \
 autoconf git coreutils automake gawk pkgconfig linux-headers libbsd-dev \
-nasm flex bison libstdc++-dev meson
+nasm flex bison libstdc++-dev meson \
+imagemagick # Because there is no png icon yet; FIXME
 
 # Build and install nasm-segelf which is a dependency of FDPP (newer versions)
 # Is this documented somewhere?
@@ -36,6 +37,18 @@ cd dosemu2
 ./default-configure --prefix=/usr
 make -j $(nproc)
 make install DESTDIR=$(readlink -f appdir) install ; find appdir/
+mkdir -p appdir/usr/share/icons/hicolor/256x256/apps/
+convert -resize 256x256 appdir/usr/share/dosemu/icons/dosemu.xpm appdir/usr/share/icons/hicolor/256x256/apps/dosemu.png
+
+# Fix desktop fie
+cat > appdir/usr/share/applications/dosemu.desktop <<\EOF
+[Desktop Entry]
+Name=dosemu2
+Comment=DOS emulator
+Exec=dosemu
+Icon=dosemu.png
+Categories=System;Emulator;
+EOF
 
 # Create AppImage
 ARCHITECTURE="x86_64" # TODO: Set based on the build system
