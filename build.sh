9fe24d3aadde5326f502be512fbc0d95cdb674c3
probonopd <probonopd@users.noreply.github.com>
2023-12-20 20:20:59 +0100

    2.0pre9

--- build.sh
+++ build.sh
@@ -13,25 +13,25 @@ apk add ca-certificates build-base wget git bash clang nasm elfutils-dev flex bi
 autoconf git coreutils automake gawk pkgconfig linux-headers libbsd-dev \
 nasm flex bison libstdc++-dev
 
-# Build and install Linuxbrew which is needed to build newer versions of fdpp
-apk add curl file gzip libc6-compat ncurses ruby ruby-etc ruby-irb ruby-json sudo
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"
-PATH=$HOME/.linuxbrew/bin:$HOME/.linuxbrew/sbin:$PATH
-brew update
-brew doctor
+# Build and install nasm-segelf which is a dependency of FDPP (newer versions)
+# Is this documented somewhere?
 
 # Build FDPP which is a dependency of dosemu2
-git clone https://github.com/dosemu2/fdpp
-cd fdpp
-# wget https://github.com/dosemu2/fdpp/archive/refs/tags/1.4.tar.gz
-# tar -zxf 1.4.tar.gz
-# cd fdpp-1.4
+# git clone https://github.com/dosemu2/fdpp
+# cd fdpp
+wget https://github.com/dosemu2/fdpp/archive/refs/tags/1.4.tar.gz
+tar -zxf 1.4.tar.gz
+cd fdpp-1.4
 make -j $(nproc)
 make install
 cd -
  
 # Build dosemu2
-git clone https://github.com/dosemu2/dosemu2
-cd dosemu2
+# git clone https://github.com/dosemu2/dosemu2
+# cd dosemu2
+wget https://github.com/dosemu2/dosemu2/archive/refs/tags/2.0pre9.tar.gz
+tar -zxf 2.0pre9.tar.gz
+cd 2.0pre9
 # autoreconf --install -v -I m4
 make -j $(nproc)
+ls -lh
