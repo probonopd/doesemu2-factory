61dcda755c23626490093ce011ceffb0442304e7
probonopd <probonopd@users.noreply.github.com>
2023-12-20 19:29:31 +0100

    Create build.sh
    [ci skip]

---
+++ build.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+set -ex
+
+if ! command -v apk; then
+	echo "This script should be run in an Alpine container"
+	exit 1
+fi
+
+# Install build dependencies
+apk update
+apk add alsa-lib-dev \
+    autoconf \
+    bash \
+    bdftopcf \
+    bison \
+    build-base \
+    git \
+    flex \
+    fontconfig \
+    libao-dev \
+    linux-headers \
+    mkfontdir \
+    sdl2-dev \
+    slang-dev \
+    strace \
+    file \
+    libexecinfo-dev \
+    gpm-dev
+
+# Build
+git clone https://github.com/dosemu2/dosemu
+cd /tmp/dosemu2
+make -j $(nproc)
