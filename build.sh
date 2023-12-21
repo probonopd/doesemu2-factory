4a042db175fac77f4ad8debc67c7535efbfe160b
probonopd <probonopd@users.noreply.github.com>
2023-12-21 12:37:44 +0100

    Get precompiled COMMAND.COM and put it into the AppDir
    https://github.com/dosemu2/dosemu2/issues/2133#issuecomment-1866096319

--- build.sh
+++ build.sh
@@ -76,6 +76,11 @@ wget -c -q https://github.com/$(wget -q https://github.com/probonopd/go-appimage
 chmod +x appimagetool-*.AppImage
 ./appimagetool-*.AppImage -s deploy ./appdir/usr/share/applications/*.desktop --appimage-extract-and-run
 
+# Get precompiled COMMAND.COM and put it into the AppDir
+mkdir -p ./appdir/usr/share/comcom32/
+wget -c -q https://dosemu2.github.io/comcom32/files/comcom32.zip
+unzip -o comcom32.zip -d ./appdir/usr/share/comcom32/
+
 # Workaround for paths to PREFIX that get compiled in at build time
 sed -i -e 's|/usr|././|g' appdir/usr/bin/dosemu
 sed -i -e 's|/usr|././|g' appdir/usr/lib/fdpp/libfdldr.so.*
